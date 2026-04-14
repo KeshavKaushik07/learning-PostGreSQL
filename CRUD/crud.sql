c63b5e9f715b5b43880db4a68d72668340bc37ce
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-04-14 17:37:24 +0530

    CRUD on table

--- CRUD/crud.sql
+++ CRUD/crud.sql
@@ -16,3 +16,17 @@ values
 (103,'jatin','Shamli'),
 (104,'abshik','Delhi'),
 (105,'ashu','Noida'); 
+
+select * from person;  -- selecting the table or use to read the data
+
+select name from person; -- selection of particular coulmn
+
+-- UPDATE
+
+update person set city = 'Noida' where id = 103;
+
+-- update person set city = 'Panipat' where id = 102;
+
+-- DELETE
+
+delete from person where id = 105;
