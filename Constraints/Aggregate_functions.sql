6bfe62515a5d00d39f91dcc097cf02e3b404c027
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-04-24 12:59:02 +0530

    count sum avg min max

---
+++ Constraints/Aggregate_functions.sql
@@ -0,0 +1,21 @@
+select * from employee;
+
+-- COUNT() ---
+
+select count(id) from employee; -- total number of recordes in a column
+
+-- SUM() ---
+
+select sum(salary) from employee; -- give total sum of all records a column 
+
+-- AVG() ---
+
+select avg(salary) from employee; -- give average number of a column
+
+-- MIN() ---
+
+select min(salary) from employee; -- minimum salary
+
+-- MAX() ---
+
+select max(salary) from employee; -- maximum salary
