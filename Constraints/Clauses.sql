3daa93c84e64edd764cfac30be9ceb191067980c
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-04-24 11:48:11 +0530

    OR AND IN NOT IN BETWEEN DISTINCT

--- Constraints/Clauses.sql
+++ Constraints/Clauses.sql
@@ -13,3 +13,23 @@ select * from employee where dept = 'Engineering' or dept ='HR';  -- OR give col
 
 select * from employee where dept = 'Engineering' and fname = 'keshav'; -- AND give result where both condition met
 
+-- IN operator
+
+select * from employee where dept IN ('IT','Engineering');
+
+--NOT IN operator
+
+select * from employee where dept NOT IN ('IT','Engineering');
+
+-- Between operator 
+
+select * from employee where salary between 40000 and 60000;
+
+-- select * from employee where salary between 40000 or 60000; can't use or with between 
+
+-- DISTINCT key ---
+
+select dept from employee; -- gives dept but can be duplicate
+
+select distinct dept from employee; -- gives only unique vales 
+
